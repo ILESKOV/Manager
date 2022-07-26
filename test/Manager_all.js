e52ec8731d8c7f477f1f40ed2b5d648cd2d59de7
Ivan Lieskov <ivan.lieskov@protonmail.com>
2022-07-26 22:29:46 +0200

    all manager tests
    all tests of manager functionality imported

---
+++ test/Manager_all.js
@@ -0,0 +1,7 @@
+describe("Manager", function () {
+  require("./Manager_depositTokens");
+  require("./Manager_whenAllDepositsMade");
+  require("./Manager_createNFT");
+  require("./Manager_createMetadata");
+  require("./Manager_withdrawTokens");
+});
