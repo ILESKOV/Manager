9bea762f0de46aa7097b86f486d6f6f682ac4a28
Ivan Leskov <103837756+ILESKOV@users.noreply.github.com>
2022-07-27 11:41:14 +0200

    tests of deposit function from Manager.sol

--- test/Manager_deposit.js
+++ test/Manager_deposit.js
@@ -13,29 +13,32 @@ beforeEach(async () => {
     let manager = await ethers.getContractFactory("Manager")
     Manager = await manager.deploy(Token.address)
 
-    await Token.transfer(user1.address, 12000)
+    await Token.transfer(user1.address, BigNumber.from("12000000000000000000000"))
 })
 
 describe("deposit tests", function () {
+    it("check deposit fee is correct", async () => {
+        expect(await Manager.connect(user1).getTokenFee()).to.equal(BigNumber.from("5000000000000000000000"))
+    })
     it("should revert if user not approved tokens for Manager contract", async () => {
         await expect(Manager.connect(user1).deposit()).to.be.revertedWith("Not enough tokens approved")
     })
     it("should revert if user approved not enough tokens for Manager contract", async () => {
-        await Token.connect(user1).approve(Manager.address, 199)
+        await Token.connect(user1).approve(Manager.address, BigNumber.from("199000000000000000000"))
         await expect(Manager.connect(user1).deposit()).to.be.revertedWith("Not enough tokens approved")
     })
     it("should revert if user already deposit tokens", async () => {
-        await Token.connect(user1).approve(Manager.address, 10000)
+        await Token.connect(user1).approve(Manager.address, BigNumber.from("10000000000000000000000"))
         await Manager.connect(user1).deposit()
         await expect(Manager.connect(user1).deposit()).to.be.revertedWith("You already deposited")
     })
     it("should check balance of user tokens change after deposit", async () => {
-        await Token.connect(user1).approve(Manager.address, 5000)
+        await Token.connect(user1).approve(Manager.address, BigNumber.from("5000000000000000000000"))
         await Manager.connect(user1).deposit()
-        expect(await Token.connect(user1).balanceOf(user1.address)).to.equal(7000)
+        expect(await Token.connect(user1).balanceOf(user1.address)).to.equal(BigNumber.from("7000000000000000000000"))
     })
     it("should check tokens are locked", async () => {
-        await Token.connect(user1).approve(Manager.address, 5000)
+        await Token.connect(user1).approve(Manager.address, BigNumber.from("5000000000000000000000"))
         await Manager.connect(user1).deposit()
         await ethers.provider.send("evm_increaseTime", [60])
         await ethers.provider.send("evm_mine")
@@ -44,22 +47,22 @@ describe("deposit tests", function () {
         timestampAfter = blockAfter.timestamp
         expect(await Manager.connect(user1).getUnlockTime(user1.address)).to.equal(timestampAfter)
     })
-    it("should check mapping of active depositors updated", async () => {
-        await Token.connect(user1).approve(Manager.address, 5000)
+    it("should check amount of active depositors increased", async () => {
+        await Token.connect(user1).approve(Manager.address, BigNumber.from("5000000000000000000000"))
         await Manager.connect(user1).deposit()
-        expect(await Manager.connect(user1).isDeposited(user1.address)).to.equal(true)
+        expect(await Manager.connect(user1).getAmountOfDepositors()).to.equal(1)
     })
     it("should check manager balance is changed in balanceOf token function", async () => {
-        await Token.connect(user1).approve(Manager.address, 5000)
+        await Token.connect(user1).approve(Manager.address, BigNumber.from("5000000000000000000000"))
         await Manager.connect(user1).deposit()
         await ethers.provider.send("evm_increaseTime", [60])
         await Manager.connect(user1).createNFT(
             "ipfs://bafybeid2iuyhqr4uxrtkyqw3x4n7ynhuteasbe4doywijmtb4qmk7bxsmm/1.png"
         )
-        expect(await Token.balanceOf(Manager.address)).to.equal(5000)
+        expect(await Token.balanceOf(Manager.address)).to.equal(BigNumber.from("5000000000000000000000"))
     })
     it("should emit an newDepositor event", async () => {
-        await Token.connect(user1).approve(Manager.address, 5000)
+        await Token.connect(user1).approve(Manager.address, BigNumber.from("5000000000000000000000"))
         blockNumAfter = await ethers.provider.getBlockNumber()
         blockAfter = await ethers.provider.getBlock(blockNumAfter)
         timestampAfter = blockAfter.timestamp
