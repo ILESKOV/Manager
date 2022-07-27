4d052e0507dad126b65cd9391dd6b78530f3b0a8
Ivan Leskov <103837756+ILESKOV@users.noreply.github.com>
2022-07-27 11:41:59 +0200

    no deposit test if all deposits have been made

--- test/Manager_whenAllDepositsMade.js
+++ test/Manager_whenAllDepositsMade.js
@@ -1,69 +1,54 @@
-const { expect } = require("chai");
-const { BigNumber } = require("ethers");
-const { ethers } = require("hardhat");
+const { expect } = require("chai")
+const { BigNumber } = require("ethers")
+const { ethers } = require("hardhat")
 
-let owner, Manager, Token;
+let owner, Manager, Token
 
 beforeEach(async () => {
-  [
-    owner,
-    user1,
-    user2,
-    user3,
-    user4,
-    user5,
-    user6,
-    user7,
-    user8,
-    user9,
-    user10,
-    user11,
-  ] = await ethers.getSigners();
+    ;[owner, user1, user2, user3, user4, user5, user6, user7, user8, user9, user10, user11] = await ethers.getSigners()
 
-  let token = await ethers.getContractFactory("Token");
-  Token = await token.deploy();
+    let token = await ethers.getContractFactory("Token")
+    Token = await token.deploy()
 
-  let manager = await ethers.getContractFactory("Manager");
-  Manager = await manager.deploy(Token.address);
+    let manager = await ethers.getContractFactory("Manager")
+    Manager = await manager.deploy(Token.address)
 
-  await Token.transfer(user1.address, 5500);
-  await Token.transfer(user2.address, 5500);
-  await Token.transfer(user3.address, 5500);
-  await Token.transfer(user4.address, 5500);
-  await Token.transfer(user5.address, 5500);
-  await Token.transfer(user6.address, 5500);
-  await Token.transfer(user7.address, 5500);
-  await Token.transfer(user8.address, 5500);
-  await Token.transfer(user9.address, 5500);
-  await Token.transfer(user10.address, 5500);
-  await Token.transfer(user11.address, 5500);
-  await Token.connect(user1).approve(Manager.address, 5000);
-  await Token.connect(user2).approve(Manager.address, 5000);
-  await Token.connect(user3).approve(Manager.address, 5000);
-  await Token.connect(user4).approve(Manager.address, 5000);
-  await Token.connect(user5).approve(Manager.address, 5000);
-  await Token.connect(user6).approve(Manager.address, 5000);
-  await Token.connect(user7).approve(Manager.address, 5000);
-  await Token.connect(user8).approve(Manager.address, 5000);
-  await Token.connect(user9).approve(Manager.address, 5000);
-  await Token.connect(user10).approve(Manager.address, 5000);
-  await Token.connect(user11).approve(Manager.address, 5000);
-  await Manager.connect(user1).deposit();
-  await Manager.connect(user2).deposit();
-  await Manager.connect(user3).deposit();
-  await Manager.connect(user4).deposit();
-  await Manager.connect(user5).deposit();
-  await Manager.connect(user6).deposit();
-  await Manager.connect(user7).deposit();
-  await Manager.connect(user8).deposit();
-  await Manager.connect(user9).deposit();
-  await Manager.connect(user10).deposit();
-});
+    await Token.transfer(user1.address, BigNumber.from("5500000000000000000000"))
+    await Token.transfer(user2.address, BigNumber.from("5500000000000000000000"))
+    await Token.transfer(user3.address, BigNumber.from("5500000000000000000000"))
+    await Token.transfer(user4.address, BigNumber.from("5500000000000000000000"))
+    await Token.transfer(user5.address, BigNumber.from("5500000000000000000000"))
+    await Token.transfer(user6.address, BigNumber.from("5500000000000000000000"))
+    await Token.transfer(user7.address, BigNumber.from("5500000000000000000000"))
+    await Token.transfer(user8.address, BigNumber.from("5500000000000000000000"))
+    await Token.transfer(user9.address, BigNumber.from("5500000000000000000000"))
+    await Token.transfer(user10.address, BigNumber.from("5500000000000000000000"))
+    await Token.transfer(user11.address, BigNumber.from("5500000000000000000000"))
+    await Token.connect(user1).approve(Manager.address, BigNumber.from("5000000000000000000000"))
+    await Token.connect(user2).approve(Manager.address, BigNumber.from("5000000000000000000000"))
+    await Token.connect(user3).approve(Manager.address, BigNumber.from("5000000000000000000000"))
+    await Token.connect(user4).approve(Manager.address, BigNumber.from("5000000000000000000000"))
+    await Token.connect(user5).approve(Manager.address, BigNumber.from("5000000000000000000000"))
+    await Token.connect(user6).approve(Manager.address, BigNumber.from("5000000000000000000000"))
+    await Token.connect(user7).approve(Manager.address, BigNumber.from("5000000000000000000000"))
+    await Token.connect(user8).approve(Manager.address, BigNumber.from("5000000000000000000000"))
+    await Token.connect(user9).approve(Manager.address, BigNumber.from("5000000000000000000000"))
+    await Token.connect(user10).approve(Manager.address, BigNumber.from("5000000000000000000000"))
+    await Token.connect(user11).approve(Manager.address, BigNumber.from("5000000000000000000000"))
+    await Manager.connect(user1).deposit()
+    await Manager.connect(user2).deposit()
+    await Manager.connect(user3).deposit()
+    await Manager.connect(user4).deposit()
+    await Manager.connect(user5).deposit()
+    await Manager.connect(user6).deposit()
+    await Manager.connect(user7).deposit()
+    await Manager.connect(user8).deposit()
+    await Manager.connect(user9).deposit()
+    await Manager.connect(user10).deposit()
+})
 
 describe("test of not allowing more than 10 depositors", function () {
-  it("check 11's depositor rejected", async () => {
-    await expect(Manager.connect(user11).deposit()).to.be.revertedWith(
-      "All deposits have been made"
-    );
-  });
-});
+    it("check 11's depositor rejected", async () => {
+        await expect(Manager.connect(user11).deposit()).to.be.revertedWith("All deposits have been made")
+    })
+})
