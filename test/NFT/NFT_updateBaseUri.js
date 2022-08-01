52884b4718cdeb67ba92138e737b1ac7ce740655
Ivan Leskov <103837756+ILESKOV@users.noreply.github.com>
2022-08-01 12:31:02 +0200

    tests of updateBaseURI function from ERC721.sol

--- test/NFT/NFT_updateBaseUri.js
+++ test/NFT/NFT_updateBaseUri.js
@@ -5,7 +5,7 @@ const { ethers } = require("hardhat")
 let owner, Manager, Token
 
 beforeEach(async () => {
-    ;[owner, user1, user2, user3] = await ethers.getSigners()
+    ;[owner, user1, user2, user3, user4, user5, user6, user7, user8, user9, user10] = await ethers.getSigners()
 
     let token = await ethers.getContractFactory("TKN")
     Token = await token.deploy()
