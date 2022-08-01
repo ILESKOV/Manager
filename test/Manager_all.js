22ea16823ce417e97ecc608442205b5f70b7e481
Ivan Leskov <103837756+ILESKOV@users.noreply.github.com>
2022-08-01 11:55:17 +0200

    Delete Manager_all.js

--- test/Manager_all.js
+++
@@ -1,7 +0,0 @@
-describe("Manager", function () {
-    require("./Manager_deposit")
-    require("./Manager_whenAllDepositsMade")
-    require("./Manager_createNFT")
-    require("./Manager_createMetadata")
-    require("./Manager_withdrawTokens")
-})
