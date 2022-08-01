cba9e1594cf9f6fb197b3e4d9f6071765febef97
Ivan Leskov <103837756+ILESKOV@users.noreply.github.com>
2022-08-01 11:41:34 +0200

    deploying script

---
+++ scripts/deploy.js
@@ -0,0 +1,39 @@
+const hre = require("hardhat")
+require("dotenv").config()
+const {
+    TKN_FEE,
+    LOCKED_PERIOD,
+    DEPOSITORS_LIMIT,
+    FIRST_GROUP_ELEMENTS,
+    SECOND_GROUP_ELEMENTS,
+    THIRD_GROUP_ELEMENTS,
+    FOURTH_GROUP_ELEMENTS,
+} = process.env
+
+async function main() {
+    const Token = await hre.ethers.getContractFactory("TKN")
+    const token = await Token.deploy()
+
+    console.log("Token deployed to:", token.address)
+
+    const Manager = await hre.ethers.getContractFactory("Manager")
+    const manager = await Manager.deploy(
+        token.address,
+        TKN_FEE,
+        LOCKED_PERIOD,
+        DEPOSITORS_LIMIT,
+        FIRST_GROUP_ELEMENTS,
+        SECOND_GROUP_ELEMENTS,
+        THIRD_GROUP_ELEMENTS,
+        FOURTH_GROUP_ELEMENTS
+    )
+
+    console.log("Manager deployed to:", manager.address)
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
