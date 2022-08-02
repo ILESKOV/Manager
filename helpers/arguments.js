de0aef6c20e6f740d566f707c131c330531b346e
Ivan Leskov <103837756+ILESKOV@users.noreply.github.com>
2022-08-02 12:14:24 +0200

    arguments for verify script

---
+++ helpers/arguments.js
@@ -0,0 +1,10 @@
+module.exports = [
+    process.env.TOKEN_ADDRESS,
+    process.env.TKN_FEE,
+    process.env.LOCKED_PERIOD,
+    process.env.DEPOSITORS_LIMIT,
+    process.env.FIRST_GROUP_ELEMENTS,
+    process.env.SECOND_GROUP_ELEMENTS,
+    process.env.THIRD_GROUP_ELEMENTS,
+    process.env.FOURTH_GROUP_ELEMENTS,
+]
